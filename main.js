8029bfb6c7a8cf7a23b635bb3980ff638b9c9c2b
bhavya <bhavya.rs@abitechnologies.in>
2021-08-05 15:52:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+let start = document.querySelector("#start")
+let result = document.querySelector("#result")
+let lang = document.querySelector("#lang")
+let recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+
+lang.addEventListener("change",(e)=>{
+    recognition.lang = e.target.value;
+})
+
+recognition.interimResults =true
+start.addEventListener("click",()=>{
+    recognition.start();
+})
+
+recognition.addEventListener("audiostart",(e)=>{
+    start.innerText ="Listening.."
+})
+
+//recognizing audio here
+recognition.addEventListener('result', (e)=> {
+    let text = '';
+    let i = 0;
+    while(i < e.results.length) {
+        text += e.results[0][0].transcript;
+        i++;
+    }
+    result.innerText = text
+})
+recognition.addEventListener("audioend",(e)=>{
+    start.innerText="Start"
+})
